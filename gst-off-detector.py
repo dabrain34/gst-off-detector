49229233b61cc33a13ee717182705adc5c0ea8ce
Stéphane Cerveau <scerveau@collabora.com>
2019-10-07 12:33:48 +0200

    gst-off-detector: able to detect and fetch info from OFF

--- gst-off-detector.py
+++ gst-off-detector.py
@@ -7,77 +7,94 @@ to a playing pipeline.
 
 import sys
 import random
-
+import openfoodfacts
 import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GLib', '2.0')
 gi.require_version('GObject', '2.0')
 from gi.repository import GLib, GObject, Gst
+from optparse import OptionParser
 
-class ProbeData:
-    def __init__(self, pipe, src):
+class GstPlayer:
+    def __init__(self, pipe, loop):
         self.pipe = pipe
-        self.src = src
+        self.loop = loop
 
-def bus_call(bus, message, loop):
+def check_off (symbol):
+    product = openfoodfacts.products.get_product(symbol)
+    print(product['status'])
+    return product
+
+def bus_call(bus, message, player):
     t = message.type
     if t == Gst.MessageType.EOS:
         sys.stdout.write("End-of-stream\n")
-        loop.quit()
+        player.loop.quit()
     elif t == Gst.MessageType.ERROR:
         err, debug = message.parse_error()
         sys.stderr.write("Error: %s: %s\n" % (err, debug))
-        loop.quit()
+        player.loop.quit()
+    elif t == Gst.MessageType.ELEMENT:
+        if message.get_structure().get_name() == "barcode":
+          print (message.get_structure())
+          print (message.get_structure().get_string("type"))
+          print (message.get_structure().get_string("symbol"))
+          #check_off("michelpatrick")
+          check_off(message.get_structure().get_string("symbol"))
+
     return True
-
-def dispose_src_cb(src):
-    src.set_state(Gst.State.NULL)
-
-def probe_cb(pad, info, pdata):
-    peer = pad.get_peer()
-    pad.unlink(peer)
-    pdata.pipe.remove(pdata.src)
-    # Can't set the state of the src to NULL from its streaming thread
-    GLib.idle_add(dispose_src_cb, pdata.src)
-
-    pdata.src = Gst.ElementFactory.make('videotestsrc')
-    pdata.src.props.pattern = random.randint(0, 24)
-    pdata.pipe.add(pdata.src)
-    srcpad = pdata.src.get_static_pad ("src")
-    srcpad.link(peer)
-    pdata.src.sync_state_with_parent()
-
-    GLib.timeout_add_seconds(1, timeout_cb, pdata)
-
-    return Gst.PadProbeReturn.REMOVE
-
-def timeout_cb(pdata):
-    srcpad = pdata.src.get_static_pad('src')
-    srcpad.add_probe(Gst.PadProbeType.IDLE, probe_cb, pdata)
-    return GLib.SOURCE_REMOVE
 
 def main(args):
     GObject.threads_init()
     Gst.init(None)
 
-    pipe =  bin = Gst.parse_launch('v4l2src name=source ! ' +
-                          'videoconvert name=zbar !' +
-                           'autovideosink')
+    parser = OptionParser()
+    parser.add_option("-c", "--custom_pipeline", dest="pipeline_str",
+                  help="Use a custom pipeline")
+    parser.add_option("-f", "--file", dest="file",
+                  help="Use file instead of v4l2src")
+    parser.add_option("-x", "--zxing",
+                  action="store_true", dest="zxing", default=False,
+                  help="use zxing instead of zbar")
+    parser.add_option("-l", "--loop",
+                  action="store_true", dest="loop", default=False,
+                  help="playback loop on eos")
+
+    (options, args) = parser.parse_args()
     
+    source = "v4l2src ! video/x-raw,width=640,height=480"
+    barcode_reader = "zbar"
+
+    if options.pipeline_str:
+      pipeline_str = options.pipeline_str
+
+    if options.zxing:
+        barcode_reader = "zxing"
+    if options.file:
+        source = "filesrc location=%s ! decodebin ! videoflip video-direction=auto" % options.file
+    pipeline_str = source + " ! videoconvert ! " + barcode_reader + " ! xvimagesink"
+
+    if options.pipeline_str:
+        pipeline_str = options.pipeline_str
+
+    print (pipeline_str)
+    pipe =  bin = Gst.parse_launch(pipeline_str)
 
     loop = GObject.MainLoop()
 
+    player = GstPlayer(pipe, loop)
+
     bus = pipe.get_bus()
     bus.add_signal_watch()
-    bus.connect ("message", bus_call, loop)
-    
+    bus.connect ("message", bus_call, player)
+
     # start play back and listen to events
     pipe.set_state(Gst.State.PLAYING)
     try:
-      loop.run()
+        loop.run()
     except:
-      pass
-    
+        pass
+
     # cleanup
     pipe.set_state(Gst.State.NULL)
 
